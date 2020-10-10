1af1a89d59336411ff2d97b6084732accfc3b77b
DESKTOP-2DEP76V\z <dan.clement1995@gmail.com>
2020-10-10 15:30:58 +0300

    Addded subtract method

--- src/main/java/ro/tefacprogramator/m4/Calculator.java
+++ src/main/java/ro/tefacprogramator/m4/Calculator.java
@@ -4,5 +4,9 @@ public class Calculator {
     public double add(double a, double b) {
         return a+b;
     }
+
+    public double subtract(double a, double b) {
+        return a-b;
+    }
     
 }
