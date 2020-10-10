526d94769bf434d99408b21d1d1a9086751d83e7
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2020-10-10 16:52:32 +0300

    Update the name of the product

--- src/main/java/ro/tefacprogramator/m4/listareproduse/ListareProduse.java
+++ src/main/java/ro/tefacprogramator/m4/listareproduse/ListareProduse.java
@@ -2,7 +2,7 @@ package ro.tefacprogramator.m4.listareproduse;
 
 public class ListareProduse {
     public String getName() {
-        String nume = "Produs1";
+        String nume = "Produs2";
         return nume;
     }
 }
